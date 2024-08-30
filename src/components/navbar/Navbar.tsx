91c7b9597a15bb395308b67f8618336e50333654
Roberdan Andrade <Roberdan.andrade.dev@gmail.com>
2024-08-30 03:29:32 -0300

    Atualização logo e template login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function Navbar() {
    return (
       <>
-         <div className="w-full flex items-center bg-[#2B4042] text-[#F5F4D6] py-2">
+         <div className="w-full flex items-center bg-[#2B4042] text-[#F5F4D6] py-3">
             <div className="w-1/3 flex justify-start px-8">
-            <Link to={'/'}><img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/Logo-Estendida.png?updatedAt=1724784792101" alt="Logo do Start Solidário" style={{width: '75px'}} /></Link>
+            <Link to={'/'}><img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/Logo-Estendida-with-text-double-font.png?updatedAt=1724785507618" alt="Logo do Start Solidário" style={{width: '225px'}} /></Link>
             </div>
 
             <div className="w-1/3 flex justify-center px-8">
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,61 +1,51 @@
-
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
-    return (
-<div className='flex bg-gradient-to-l from-[#C2E0CA] via-[#F5F4D6] to-[#C2E0CA] '>
-    <div><img src='https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/diferentes-pessoas-fazendo-trabalho-voluntario_23-2149012164.png?updatedAt=1724776780689'></img></div>
-    <div className='bg-[#2B4042]'>
-        <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" >
-          <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-          <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuário</label>
-            <input
-              type="text"
-              id="usuario"
-              name="usuario"
-              placeholder="Usuario"
-              className="border-2 border-slate-700 rounded p-2"
-              
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              placeholder="Senha"
-              className="border-2 border-slate-700 rounded p-2"
-              
-            />
-          </div>
-          <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
-           
-        
-          
-            <span>Entrar</span>
-          </button>
+   return (
+      <div className='flex justify-center justify-items-center h-[80vh] bg-[#F5F4D6]'>
+         <div className='flex rounded-3xl border-2 border-black w-8/12 m-auto'>
+            <div className='w-3/5'>
+               <img className='rounded-l-3xl w-full h-full border-r-2 border-black' src='https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/diferentes-pessoas-fazendo-trabalho-voluntario_23-2149012164.png?updatedAt=1724776780689' />
+            </div>
 
-          <hr className="border-slate-800 w-full" />
+            <div className='w-2/5'>
+               <form className="w-full h-full rounded-r-3xl bg-[#1E765A] text-[#F5F4D6]" >
+                  <h2 className="text-3xl flex justify-center justify-items-center py-4">Entre na sua conta:</h2>
+                  <div className="flex flex-col w-full px-4 pb-2">
+                     <label htmlFor="usuario" className='px-1 text-lg'>Usuário:</label>
+                     <input
+                        type="text"
+                        id="usuario"
+                        name="usuario"
+                        placeholder="Usuario"
+                        className="rounded-xl p-2"
+                     />
+                  </div>
+                  <div className="flex flex-col w-full px-4 pt-2">
+                     <label htmlFor="senha" className='px-1 text-lg'>Senha:</label>
+                     <input
+                        type="password"
+                        id="senha"
+                        name="senha"
+                        placeholder="Senha"
+                        className="rounded-xl p-2"
+                     />
+                  </div>
 
-          <p>
-            Ainda não tem uma conta?{' '}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
-              Cadastre-se
-            </Link>
-          </p>
-        </form>
-        <div className="fundoLogin hidden lg:block"></div>
+                  <button type='submit' className="flex justify-center justify-items-center w-full text-xl py-4">
+                     <span className='hover:underline'>Entrar</span>
+                  </button>
+
+                  <p className='flex justify-center justify-items-center w-full'>
+                     Ainda não tem uma conta?
+                     <Link to="/cadastro" className="hover:underline pl-2">Cadastre-se Agora!</Link>
+                  </p>
+               </form>
+            </div>
+         </div>
       </div>
-    </div>
-        </div>
-    </div>
+   );
+}
 
-      );
-    }
-    
-    export default Login;
+export default Login;
