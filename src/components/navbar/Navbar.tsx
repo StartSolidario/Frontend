e7d8b0637b8d3681fc3ab7950ff985ff2eea1402
Roberdan Andrade <Roberdan.andrade.dev@gmail.com>
2024-09-02 02:29:47 -0300

    Atualizando titulo e favicon; Atualizando width Navbar ;Removendo CSS inutilizado

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,11 +4,11 @@ function Navbar() {
    return (
       <>
          <div className="w-full flex items-center bg-[#2B4042] text-[#F5F4D6] py-3">
-            <div className="w-1/3 flex justify-start px-8">
+            <div className="w-[20%] flex justify-start px-8">
             <Link to={'/'}><img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/Logo-Estendida-with-text-double-font.png?updatedAt=1724785507618" alt="Logo do Start Solidário" style={{width: '225px'}} /></Link>
             </div>
 
-            <div className="w-1/3 flex justify-center px-8">
+            <div className="w-[60%] flex justify-center px-8">
                <ul className="flex">
                   <li><Link to={'/voluntariado'} className="px-5 text-3xl">Voluntariado</Link></li>
                   <li><Link to={'/comprar'} className="px-5 text-3xl">Comprar</Link></li>
@@ -16,7 +16,7 @@ function Navbar() {
                </ul>
             </div>
 
-            <div className="w-1/3 flex justify-end px-8 text-3xl">
+            <div className="w-[20%] flex justify-end px-8 text-3xl">
                <Link to={'/login'}>Login</Link>
             </div>
          </div>
