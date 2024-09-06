b79a2aaf1ec016b426cbc226814aa9febee5c503
jeansdev1 <jeanv_lima@outlook.com>
2024-09-06 18:29:48 -0300

    alteracao do sobre e contato com hover

--- src/pages/equipecontato/EquipeContato.tsx
+++ src/pages/equipecontato/EquipeContato.tsx
@@ -4,8 +4,8 @@ function EquipeContato() {
     return (
         <div className="bg-[#F5F4D6]">
             <div className="grid grid-cols-2 py-16 px-20">
-                <div className="bg-[#518E48] border-4 rounded-l-lg border-black justify-center">
-                    <form id="formulario" action="https://formsubmit.co/d425c3abca8e6edad333b3d489546a89" method="POST" className="grid grid-cols-1 flex item-center justify-center ">
+                <div className="bg-[#364d33] border-4 rounded-l-lg border-black justify-center">
+                    <form id="formulario" action="https://formsubmit.co/d425c3abca8e6edad333b3d489546a89" method="POST" className="grid grid-cols-1 item-center justify-center ">
 
                         <input type="hidden" name="_next" value="https://roberdan-andrade.github.io/Portfolio_Generation/sucess.html" />
                         <input type="hidden" name="_captcha" value="false" />
@@ -35,9 +35,9 @@ function EquipeContato() {
                 </div>
             </div>
             <div>
-                <h2 className="flex item-center justify-center font-bold p-6">Conheça nossa equipe!</h2>
+                <h2 className="flex item-center justify-center font-bold p-6 text-4xl">Conheça nossa equipe!</h2>
                 <div className="grid grid-cols-3 gap-6 p-6">
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Captura%20de%20tela%202024-09-06%20141234.png?updatedAt=1725642832805"
@@ -55,7 +55,7 @@ function EquipeContato() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Imagem%20do%20WhatsApp%20de%202024-09-06%20%C3%A0(s)%2012.05.38_953507ed.jpg?updatedAt=1725642831841"
@@ -73,7 +73,7 @@ function EquipeContato() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Imagem%20do%20WhatsApp%20de%202024-09-06%20%C3%A0(s)%2011.57.42_df836fd8.jpg?updatedAt=1725642833406"
@@ -91,7 +91,7 @@ function EquipeContato() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Imagem%20do%20WhatsApp%20de%202024-09-06%20%C3%A0(s)%2011.57.36_4e8e7579.jpg?updatedAt=1725642834341"
@@ -109,7 +109,7 @@ function EquipeContato() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Imagem%20do%20WhatsApp%20de%202024-09-06%20%C3%A0(s)%2012.24.14_284239f7.jpg?updatedAt=1725642831558"
@@ -127,7 +127,7 @@ function EquipeContato() {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6">
+                    <div className="flex flex-col items-center bg-[#2B4042] shadow-lg rounded-lg p-6 hover:bg-[#1e5353]">
                         <div className="w-24 h-24 rounded-full overflow-hidden bg-white">
                             <img
                                 src="https://ik.imagekit.io/startsolidario/fotos%20equipe/Imagem%20do%20WhatsApp%20de%202024-09-06%20%C3%A0(s)%2012.07.58_f1ac2170.jpg?updatedAt=1725642832098"
--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,33 +1,45 @@
 function Sobre() {
-  return (
-    <>
-    <div className="bg-[#324C4F] p-6 min-h-screen flex flex-col items-center"> {/*Body*/}
-        <div className="flex flex-col lg:flex-row items-center justify-center w-full my-8 font-jura text-white px-4 lg:px-8"> {/*Sessão sobre*/}
-            <div> {/*Sobre - Texto*/}
-                <h1 className="font-bold text-3xl lg:text-4x1 text-center mb-6"> Como Voce Pode Fazer a Diferença no StartSolidario?</h1>
-                <p className="text-lg lg:text-xl"> Aqui, você não apenas adquire produtos ou pacotes de viagem, você participa de algo maior. Cada compra e cada gesto de doação reverberam em impacto social, apoiando causas que fazem a diferença na vida de muitas pessoas. Seja comprando para si ou doando para os outros, você está investindo em um futuro mais justo e inclusivo..</p>
+    return (
+        <>
+            <div className="bg-[#1d2c2e] flex flex-col items-center p-20">
+                <h1 className="text-white font-bold text-center text-5xl mb-4" > O que é o StartSolidário?</h1>
 
+                <div className="flex items-center justify-between max-w-4xl w-full">
+                    <p className="text-white text-left text-xl"> O Start Solidário é muito mais que um e-commerce,
+                        é um movimento que une solidariedade e propósito. Nossa plataforma
+                        conecta quem quer ajudar com quem mais precisa, oferecendo a oportunidade de
+                        transformar vidas através de doações, voluntariado e compras com significado.
+                    </p>
+                    <img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/Logo-Estendida.png?updatedAt=1724784792101" alt="Logotipo" className="w-44 h-32 ml-8" ></img>
+                </div>
             </div>
+            <div className="bg-[#324C4F] p-6 min-h-screen flex flex-col items-center"> {/*Body*/}
+                <div className="flex lg:flex-row items-center justify-center my-8 font-jura text-white px-4 lg:px-8"> {/*Sessão sobre*/}
+                    <div> {/*Sobre - Texto*/}
+                        <h1 className="font-bold text-3xl lg:text-4x1 text-center mb-6"> Como Voce Pode Fazer a Diferença no StartSolidario?</h1>
+                        <p className="text-lg text-start"> Aqui, você não apenas adquire produtos ou pacotes de viagem, você participa de algo maior. Cada compra e cada gesto de doação reverberam em impacto social, apoiando causas que fazem a diferença na vida de muitas pessoas. Seja comprando para si ou doando para os outros, você está investindo em um futuro mais justo e inclusivo..</p>
 
-            <div className=" mx-auto w-2/3 border-current border-2 rounded"> {/*Sobre - Imagem*/}
-                <img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/feche-pessoas-sorridentes.jpg?updatedAt=1724778221364"></img>
-            </div>
-        </div>
-        
-        <div className="justify-center grid grid-cols-2"> {/*Sessão ajuda*/}
-            <div> {/*ajuda - Imagem*/}
-                <img className="mx-auto flex flitems-cenwhite border-2 rounded" src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/pessoas-felizes-se-oferecendo-para-causas-especiais_23-2148687314.png" />
-            </div>
-            
-            <div className="flex-1"> {/*ajuda - Texto*/}
-                <h1 className="font-bold text-2xl p-6 text-center font-jura text-white">Nosso Propósito</h1>
-                <h3 className="text-lg leading-relaxed p-6 text-center font-jura text-white">  Nosso propósito é criar um ambiente onde a solidariedade seja acessível e tangível, permitindo que todos possam contribuir com o que têm de melhor: seu tempo, recursos ou disposição para ajudar. Faça parte dessa corrente do bem e descubra como pequenas ações podem gerar grandes mudanças.</h3>
+                    </div>
 
+                    <div className=" mx-auto w-2/3 border-current border-2 rounded"> {/*Sobre - Imagem*/}
+                        <img src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/feche-pessoas-sorridentes.jpg?updatedAt=1724778221364"></img>
+                    </div>
+                </div>
+
+                <div className="justify-center grid grid-cols-2"> {/*Sessão ajuda*/}
+                    <div> {/*ajuda - Imagem*/}
+                        <img className="mx-auto flex flitems-cenwhite border-2 rounded" src="https://ik.imagekit.io/startsolidario/STARTSOLIDARIO/pessoas-felizes-se-oferecendo-para-causas-especiais_23-2148687314.png" />
+                    </div>
+
+                    <div className="flex-1"> {/*ajuda - Texto*/}
+                        <h1 className="font-bold text-2xl p-6 text-center font-jura text-white">Nosso Propósito</h1>
+                        <h3 className="text-lg leading-relaxed p-6 text-center font-jura text-white">  Nosso propósito é criar um ambiente onde a solidariedade seja acessível e tangível, permitindo que todos possam contribuir com o que têm de melhor: seu tempo, recursos ou disposição para ajudar. Faça parte dessa corrente do bem e descubra como pequenas ações podem gerar grandes mudanças.</h3>
+
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Sobre
